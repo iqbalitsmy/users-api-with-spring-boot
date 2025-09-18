bea56dded0bcbc24934c9170c216ad06f8873984
tatamax <tatamax0711@gmail.com>
2025-09-19 01:02:14 +0600

    spring project setup

---
+++ src/main/java/com/iqbalitsmy/users_api_with_spring_boot/UsersApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.iqbalitsmy.users_api_with_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iqbalitsmy/users_api_with_spring_boot/UsersApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqbalitsmy.users_api_with_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
